0bb5fd8884d48d5317a6981e70212fd112bd67a5
Samrudh M gowda <samrudhmgowda61@gmail.com>
2025-02-24 19:01:19 +0530

    FIx

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import { IoCart } from "react-icons/io5";
 
-
 const Navbar = () => (
-  <nav className="flex justify-around items-center py-4 font-sans ">
+  <nav className="flex justify-around items-center py-4 font-sans">
     <h1 className="text-2xl font-semibold font-mono">
       <Link to="/">Food-Explorer</Link>
     </h1>
     <div>
-      <Link to="/cart" className="text-lg font-semibold flex justify-center items-center space-x-1.5 p-1.5">Cart <IoCart size={20} />
+      <Link
+        to="/cart"
+        className="text-lg font-semibold flex justify-center items-center space-x-1.5 p-1.5"
+      >
+        Cart <IoCart size={20} />
       </Link>
     </div>
   </nav>
